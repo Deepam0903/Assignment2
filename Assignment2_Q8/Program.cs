9553bab788b56ddf9b8815a4fdf582a46a531751
Deepam0903 <59901280+Deepam0903@users.noreply.github.com>
2020-02-23 16:48:55 -0500

    Update Program.cs

--- Assignment2_Q8/Program.cs
+++ Assignment2_Q8/Program.cs
@@ -10,30 +10,31 @@ namespace Assignment2_Q8
             string keywords = "uaf";
             var result = DictSearch(userDict, keywords);
             Console.WriteLine(result);
-
-            Console.ReadKey();
         }
 
-        public static bool DictSearch(string[] userDict, string keyword)
-        {
-            foreach (var item in userDict)
-            {
-                //temp is a counter which will check the no. of letters matched. If more than 2 then condition will fail
-                //index is for the string's index
-                int temp = 0, index = 0;
-                while (temp < 2 && index < item.Length && index < keyword.Length)
-                {
-                    if (item[index] != keyword[index] && item.Length == keyword.Length)  
-                        //if the keyword index is not equal to item's index and 
-                        //Length of both should be same. 
-                        temp++;
-                    index++;
-                }
-                if (temp < 2 && temp != 0)
-                    return true;
-            }
-            return false;
-        }
+         public static bool DictSearch(string[] userDict, string keyword)
+          {
+             try
+             {
+
+             foreach (var item in userDict)
+              {
+                  //temp is a counter which will check the no. of letters matched. If more than 2 then condition will fail
+                  //index is for the string's index
+                  int temp = 0, index = 0;
+                  while (temp < 2 && index < item.Length && index < keyword.Length)
+                  {
+                      if (item[index] != keyword[index] && item.Length == keyword.Length)  
+                          //if the keyword index is not equal to item's index and 
+                          //Length of both should be same. 
+                          temp++;
+                      index++;
+                  }
+                  if (temp < 2 && temp != 0)
+                      return true;
+              }
+              return false;
+          }
     }
 }
     
